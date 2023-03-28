6f26e7fe8f71d9a305787f3d506fcd9146db2de4
gmberton <bertongabri@gmail.com>
2023-03-28 14:36:17 +0200

    Download all datasets at once

--- download_datasets.py
+++ download_datasets.py
@@ -11,9 +11,9 @@ import shutil
 
 os.makedirs("data", exist_ok=True)
 for dataset_name, url in URLS.items():
+    print(f"Downloading {dataset_name}")
     zip_filepath = f"data/{dataset_name}.zip"
     gdown.download(url, zip_filepath, fuzzy=True)
     shutil.unpack_archive(zip_filepath, extract_dir="data")
     os.remove(zip_filepath)
-    break
 
