0cd4ef838358194b342ba8f88394440abb08ac4b
gmberton <bertongabri@gmail.com>
2023-03-28 12:19:38 +0200

    Improved parser descriptions

--- parser.py
+++ parser.py
@@ -4,16 +4,38 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--batch_size", type=int, default=64, help="_")
-    parser.add_argument("--img_per_place", type=int, default=4, help="_")
-    parser.add_argument("--min_img_per_place", type=int, default=4, help="_")
-    parser.add_argument("--descriptors_dim", type=int, default=512, help="_")
-    parser.add_argument("--max_epochs", type=int, default=20, help="_")
-    parser.add_argument("--num_workers", type=int, default=16, help="_")
+    # Training parameters
+    parser.add_argument("--batch_size", type=int, default=64,
+                        help="The number of places to use per iteration (one place is N images)")
+    parser.add_argument("--img_per_place", type=int, default=4,
+                        help="The effective batch size is (batch_size * img_per_place)")
+    parser.add_argument("--min_img_per_place", type=int, default=4,
+                        help="places with less than min_img_per_place are removed")
+    parser.add_argument("--max_epochs", type=int, default=20,
+                        help="stop when training reaches max_epochs")
+    parser.add_argument("--num_workers", type=int, default=8,
+                        help="number of processes to use for data loading / preprocessing")
 
-    parser.add_argument("--train_path", type=str, required=True, help="path to train set")
-    parser.add_argument("--val_path", type=str, required=True, help="path to val set")
-    parser.add_argument("--test_path", type=str, required=True, help="path to test set")
+    # Architecture parameters
+    parser.add_argument("--descriptors_dim", type=int, default=512,
+                        help="dimensionality of the output descriptors")
+    
+    # Visualizations parameters
+    parser.add_argument("--num_preds_to_save", type=int, default=0,
+                        help="At the end of training, save N preds for each query. "
+                        "Try with a small number like 3")
+    parser.add_argument("--save_only_wrong_preds", action="store_true",
+                        help="When saving preds (if num_preds_to_save != 0) save only "
+                        "preds for difficult queries, i.e. with uncorrect first prediction")
+
+    # Paths parameters
+    parser.add_argument("--train_path", type=str, required=True,
+                        help="path to train set")
+    parser.add_argument("--val_path", type=str, required=True,
+                        help="path to val set (must contain database and queries)")
+    parser.add_argument("--test_path", type=str, required=True,
+                        help="path to test set (must contain database and queries)")
+    
     args = parser.parse_args()
     return args
 
